50b188e59ecca33f996652cec9835f04d4d94f85
Yoiner Duran <duranyoiner@gmail.com>
2023-07-10 23:07:34 -0500

    resubiendo azure storage

--- config/settings.py
+++ config/settings.py
@@ -41,11 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #libs
     'widget_tweaks',
+    'storages',
     #Mis apps
-    'apps.blogs',
     'apps.dispositivos',
     'apps.juegos',
-    'apps.usuarios'
+    'apps.usuarios',
+    'apps.blogs',
 ]
 
 MIDDLEWARE = [
@@ -128,7 +129,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
 # configurando carpeta de mis archivos estaticos
 STATICFILES_DIRS = [
   os.path.join(BASE_DIR,"static")
@@ -140,7 +140,7 @@ STATIC_URL = '/static/'
 
 #media
 #carpeta donde se van a guardar los archivos subidos
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 #link de como se accede a ellos de forma publica
 MEDIA_URL = 'media/'
