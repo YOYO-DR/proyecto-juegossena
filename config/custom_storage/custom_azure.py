44b0cfc6661aff325eedba4bf6829dbaf54f87d4
Yoiner Duran <duranyoiner@gmail.com>
2023-07-10 22:37:59 -0500

    agregando azure storage

---
+++ config/custom_storage/custom_azure.py
@@ -0,0 +1,17 @@
+from storages.backends.azure_storage import AzureStorage
+from config.settings import azure_storage_blob_parametros
+    
+class PublicAzureStaticStorage(AzureStorage):
+    account_name = azure_storage_blob_parametros['account_name']
+    account_key = azure_storage_blob_parametros['account_key']
+    azure_container = azure_storage_blob_parametros['container_name']
+    location = 'static/'  # ubicación dentro del contenedor
+    expiration_secs = None
+
+# Configuración para los archivos de media
+class PublicAzureMediaStorage(AzureStorage):
+    account_name = azure_storage_blob_parametros['account_name']
+    account_key = azure_storage_blob_parametros['account_key']
+    azure_container = azure_storage_blob_parametros['container_name']
+    location = 'media/'  # ubicación dentro del contenedor
+    expiration_secs = None
--- config/settings.py
+++ config/settings.py
@@ -179,3 +179,23 @@ EMAIL_HOST_USER = config_email_tienda_valores['email']
 # la contraseña
 EMAIL_HOST_PASSWORD = config_email_tienda_valores['password']
 EMAIL_USE_TLS = True
+
+#almacenmiento azure
+# Configuración para el diccionario de storages
+# si estamos en producción o desarrollo, y saber de donde traer la configuración
+if 'WEBSITE_HOSTNAME' in os.environ: 
+    azure_storage_blob = os.environ['AZURE_STORAGE_BLOB']
+    azure_storage_blob_parametros = {parte.split(' = ')[0]:parte.split(' = ')[1] for parte in azure_storage_blob.split('  ')}
+else:
+    azure_storage_blob_parametros = {'account_name':os.environ.get('ACCOUNT_NAME'),
+                                     'container_name':os.environ.get('CONTAINER_NAME'),
+                                     'account_key':os.environ.get('ACCOUNT_KEY')}
+
+AZURE_CONTAINER = azure_storage_blob_parametros['container_name']
+AZURE_ACCOUNT_NAME = azure_storage_blob_parametros['account_name']
+AZURE_ACCOUNT_KEY = azure_storage_blob_parametros['account_key']
+STORAGES = {
+    "default": {"BACKEND": "storages.backends.azure_storage.AzureStorage"},
+    "staticfiles": {"BACKEND": "custom_storage.custom_azure.PublicAzureStaticStorage"},
+    "media": {"BACKEND": "custom_storage.custom_azure.PublicAzureMediaStorage"},
+}
