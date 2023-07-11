54818a3ec79286140764696812ea1196da4b0c13
Yoiner Duran <duranyoiner@gmail.com>
2023-07-11 12:10:47 -0500

    agregando imagen en modal a los requerimientos

--- apps/blogs/tests.py
+++
@@ -1,6 +0,0 @@
-# python manage.py test apps.blogs
-from config.wsgi import *
-from apps.blogs.models import Requerimientos
-
-for i in Requerimientos.objects.all():
-    print(f'Requerimiento: {i.nombre}')
