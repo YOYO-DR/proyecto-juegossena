d164cde1054b523df971a3fa7c40b427f19814ec
Yoiner Duran <duranyoiner86@gmail.com>
2023-08-09 14:05:35 -0500

    session

--- apps/apiAndroid/views.py
+++ apps/apiAndroid/views.py
@@ -139,7 +139,7 @@ class DatosUsuarioApi(View):
     #Verifico la sesion
     id_usuario=verificar_sesion(request.POST.get('session_id'))
     if id_usuario is None:
-       data['error']="Sesión invalida"
+       data['error']="sesion invalida"
        return JsonResponse(data)
     
     usuario=Usuario.objects.get(id=id_usuario)
