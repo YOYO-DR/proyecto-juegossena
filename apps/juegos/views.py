f1a7108b9167b622e5b1746b7c4c74e90ea4e5ce
Yoiner Duran <duranyoiner@gmail.com>
2023-08-14 06:39:42 -0500

    maquetando inicio

--- apps/juegos/views.py
+++ apps/juegos/views.py
@@ -3,12 +3,11 @@ from django.views.generic import TemplateView
 from apps.dispositivos.models import Juegos
 
 class InicioView(TemplateView): 
-  template_name = 'juegos/inicio.html'
+  template_name = 'index.html'
 
   def get_context_data(self, **kwargs):
       context = super().get_context_data(**kwargs)
       context["titulo"] = 'Inicio'
-      context["dato"] = 'Este va a ser la pagina inicial del proyecto'
       return context
 
 
