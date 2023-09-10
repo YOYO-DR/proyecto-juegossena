b80d6a773e6b786e73bc24f0f2f5f3fc27c2c7bf
Yoiner Duran <duranyoiner@gmail.com>
2023-09-10 01:23:41 -0500

    haciendo animacion de seleccion del radio input de los dispositivos

--- apps/juegos/static/js/buscar_juegos.js
+++ apps/juegos/static/js/buscar_juegos.js
@@ -1,9 +1,30 @@
 //constantes
 const radioInputs = document.querySelectorAll('input[name="dispo');
 
+//funciones
+function desRadios() {
+  radioInputs.forEach((radio) => {
+    if (!radio.checked) {
+      radio.disabled = true;
+      radio.parentNode.classList.add("disabled")
+    }
+  });
+}
+
+function actRadios() {
+  radioInputs.forEach((radio) => {
+    if (!radio.checked) {
+      radio.disabled = false;
+      radio.parentNode.classList.remove("disabled");
+    }
+  });
+}
+
 //evento de cuando se seleccione un radio
 radioInputs.forEach((radio) => {
   radio.addEventListener("change", (e) => {
+    //desactivo los demás radios
+    desRadios();
     //verifico si el radio esta seleccionado
     if (radio.checked) {
       const label = document.querySelector(
@@ -12,9 +33,12 @@ radioInputs.forEach((radio) => {
       radio.disabled=true
       const spn = spnCargando()
       label.appendChild(spn)
+      //simulando peticion
       setTimeout(() => {
         label.removeChild(spn)
         radio.disabled = false
+        //activar radios
+        actRadios();
       },2000)
     }
   });
