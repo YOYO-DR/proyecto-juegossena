da58dba511bc34c30a78a76ca34b4aacd37ac3b9
Yoiner Duran <duranyoiner@gmail.com>
2023-09-10 16:00:30 -0500

    agregando botones para la busqueda de los juegos

--- apps/juegos/static/js/buscar_juegos.js
+++ apps/juegos/static/js/buscar_juegos.js
@@ -1,45 +1,49 @@
-//constantes
-const radioInputs = document.querySelectorAll('input[name="dispo');
-
 //funciones
-function desRadios() {
+function actDesRadios(radioInputs,activar = true) {
   radioInputs.forEach((radio) => {
     if (!radio.checked) {
-      radio.disabled = true;
-      radio.parentNode.classList.add("disabled")
+      radio.disabled = activar == true ? false : true;
+      if (activar == true) {
+        radio.parentNode.classList.remove("disabled");
+      } else {
+        radio.parentNode.classList.add("disabled");
+      }
     }
   });
 }
 
-function actRadios() {
-  radioInputs.forEach((radio) => {
-    if (!radio.checked) {
-      radio.disabled = false;
-      radio.parentNode.classList.remove("disabled");
-    }
-  });
-}
+document.addEventListener("DOMContentLoaded", (e) => {
+  //constantes
+  const radioInputs = document.querySelectorAll('input[name="dispo');
+  const opciones = document.querySelector(".opciones");
+  const botonOpciones = opciones.querySelector("button");
 
-//evento de cuando se seleccione un radio
-radioInputs.forEach((radio) => {
-  radio.addEventListener("change", (e) => {
-    //desactivo los demás radios
-    desRadios();
-    //verifico si el radio esta seleccionado
-    if (radio.checked) {
-      const label = document.querySelector(
-        `label[for="${radio.getAttribute("id")}"]`
-      );
-      radio.disabled=true
-      const spn = spnCargando()
-      label.appendChild(spn)
-      //simulando peticion
-      setTimeout(() => {
-        label.removeChild(spn)
-        radio.disabled = false
-        //activar radios
-        actRadios();
-      },2000)
-    }
+  //evento de cuando se seleccione un radio
+  radioInputs.forEach((radio) => {
+    radio.addEventListener("change", (e) => {
+      //desactivo los demás radios
+      actDesRadios(radioInputs, (activar = false));
+      //verifico si el radio esta seleccionado
+      if (radio.checked) {
+        const label = document.querySelector(
+          `label[for="${radio.getAttribute("id")}"]`
+        );
+        radio.disabled = true;
+        const spn = spnCargando();
+        label.appendChild(spn);
+        //simulando peticion
+        setTimeout(() => {
+          label.removeChild(spn);
+          radio.disabled = false;
+          //activar radios
+          actDesRadios(radioInputs, (activar = true));
+        }, 2000);
+      }
+    });
   });
-});
+
+  //boton de las opciones
+  botonOpciones.addEventListener("click", (e) => {
+    opciones.classList.toggle("activa");
+  });
+})
