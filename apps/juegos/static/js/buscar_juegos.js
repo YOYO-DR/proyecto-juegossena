ca0948cc13797e367e09d581c9ab3a98d20ebdad
Yoiner Duran <duranyoiner@gmail.com>
2023-09-11 13:05:45 -0500

    manejando funcion de busqueda

--- apps/juegos/static/js/buscar_juegos.js
+++ apps/juegos/static/js/buscar_juegos.js
@@ -21,12 +21,69 @@ function heightCheckbox(checkbox) {
   return sumaHeight;
 }
 
+//realizar busqueda
+function buscar(check, input) {
+  //(check,radios,input)
+  //check=id del checkbox del evento
+  //radios = [lista y pregunto cual es el precionado ya que solo seria 1]
+  //input = div.input-group de la busqueda > input y button{i del icono}
+
+  const ckeckboxs = document.querySelectorAll(`input[type="checkbox"]`);
+
+  //ejecuto la busqueda solo si el input tiene datos
+  if (input.value.trim()) {
+    //buscar
+    //desactivar
+    //spn del check
+    let spnCheck = spnCargando();
+    ckeckboxs.forEach((checkbox) => {
+      if (checkbox.getAttribute("id") == check) {
+        //le agrego el spinner
+        checkbox.parentNode.querySelector("label").appendChild(spnCheck);
+      }
+      checkbox.disabled = true;
+    });
+
+    //desactivar input
+    input.disabled = true;
+
+    //hacer peticion
+    setTimeout(() => {
+      //activar checkboxs
+      ckeckboxs.forEach((checkbox) => {
+        if (checkbox.getAttribute("id") == check) {
+          checkbox.parentNode.querySelector("label").removeChild(spnCheck);
+        }
+        checkbox.disabled = false;
+        input.disabled = false;
+      });
+    }, 2000);
+  } else {
+    mensajeSweet("Debe ingresar un valor en la busqueda","warning");
+  }
+}
+
 document.addEventListener("DOMContentLoaded", (e) => {
   //constantes
   const radioInputs = document.querySelectorAll('input[name="dispo');
   const opciones = document.querySelector(".opciones");
   const botonOpciones = opciones.querySelector("button");
+  const checkboxs = document.querySelectorAll(".form-check .form-check-input");
+  const inputBuscar = document.querySelector("#inputBusqueda");
   let sumaCheckbox = heightCheckbox(opciones.querySelectorAll(".form-check"));
+
+  //evento de los checkboxs
+  checkboxs.forEach((checkbox) => {
+    checkbox.addEventListener("click", (e) => {
+      if (!inputBuscar.value.trim()) {
+        //si el input etsa vacio, no dejo que lo marque
+        checkbox.checked = !checkbox.checked;
+      }
+      buscar(checkbox.getAttribute("id"), inputBuscar);
+    })
+  }
+  );
+
   //evento de cuando se seleccione un radio
   radioInputs.forEach((radio) => {
     radio.addEventListener("change", (e) => {
@@ -34,6 +91,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
       actDesRadios(radioInputs, (activar = false));
       //verifico si el radio esta seleccionado
       if (radio.checked) {
+
         const label = document.querySelector(
           `label[for="${radio.getAttribute("id")}"]`
         );
